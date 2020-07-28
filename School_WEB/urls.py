93a9a5a4129cbe3e5c0ec7a80a3c7e54e95c557c
Jonckers <etiennejonckers@gmail.com>
2020-07-28 10:55:12 +0200

    dashboardParent.html

--- School_WEB/urls.py
+++ School_WEB/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url
 from django.urls import path
-
 from School_WEB import views
 
 urlpatterns = [
-    url(r'^', views.home, name='home'),
+    url(r'^home', views.home, name='home'),
+    url(r'^dashboardParent/', views.dashboardParent, name='dashboardParent'),
 
     ]
--- School_WEB/views.py
+++ School_WEB/views.py
@@ -5,3 +5,7 @@ def home(request):
 
     return render(request, 'web/home.html')
 
+def dashboardParent(request):
+
+    return render(request, 'web/dashboardParent.html')
+
